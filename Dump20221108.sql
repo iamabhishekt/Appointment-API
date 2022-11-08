032ddb8a7bca821036db86473e59014a886eeaff
RiniCatherineJoseph <105745165+RiniCatherineJoseph@users.noreply.github.com>
2022-11-08 11:10:11 -0500

    Postman and sql for testing

---
+++ Dump20221108.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE  IF NOT EXISTS `appointment` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `appointment`;
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+--
+-- Host: localhost    Database: appointment
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appointment_list`
+--
+
+DROP TABLE IF EXISTS `appointment_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointment_list` (
+  `appointmentId` varchar(45) NOT NULL,
+  `patientId` varchar(45) DEFAULT NULL,
+  `patientName` varchar(45) DEFAULT NULL,
+  `doctorId` varchar(45) DEFAULT NULL,
+  `doctorName` varchar(45) DEFAULT NULL,
+  `appointmentDate` varchar(45) DEFAULT NULL,
+  `aDuration` varchar(45) DEFAULT NULL,
+  `aStatus` varchar(45) DEFAULT NULL,
+  `aType` varchar(45) DEFAULT NULL,
+  `caseManagerId` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`appointmentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appointment_list`
+--
+
+LOCK TABLES `appointment_list` WRITE;
+/*!40000 ALTER TABLE `appointment_list` DISABLE KEYS */;
+INSERT INTO `appointment_list` VALUES ('1','12','John Doe','123','Stepehen','11/03/2022','30','True','Virtual','1234'),('bdce0301-95e7-4206-a894-91cfb01da69b','12345','test','22345','Test Doctor','07/03/2022','30','True','virtual','1111');
+/*!40000 ALTER TABLE `appointment_list` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-08 11:06:56
